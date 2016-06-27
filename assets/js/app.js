f71b5d6d6533db30f57d56d9fb47f3f30b4e379c
japolinar <jessica.apolinar@globalcorporation.cc>
2016-06-27 13:27:53 -0500

    busqueda podcast

--- assets/js/app.js
+++ assets/js/app.js
@@ -26,15 +26,33 @@ angular.module('podcastApp',['ui.router','main.module', 'servicesModule'])
 					controller: 'mainController'
 				})
 	}])
-	.controller('mainController', ['$scope', '$rootScope', '$http','podcastService', '$q', function ( $scope, $rootScope, $http, podcastService, $q) {
+	.controller('mainController', ['$scope', '$rootScope', '$http','podcastService', '$q', '$sce', function ( $scope, $rootScope, $http, podcastService, $q, $sce) {
 		$scope.prueba2 = "Es una prueba y debe de funcionar";
 		$scope.baseUrl = "https://itunes.apple.com/";
 		$scope.params = new Object();
 
-    
+    	$scope.pais = [{name: "Argentina", value:"ar"}, {name: "Australia", value:"au"}, {name: "Belize", value:"bz"}, {name: "Canada", value:"ca"},
+    				   {name: "China", value:"cn"}, {name: "Colombia", value:"co"},{name: "Estados Unidos", value:"us"}, {name: "España", value:"es"},
+    				   {name: "Francia", value:"fr"}, {name: "Grecia", value:"gr"}, {name: "Italia", value:"it"},{name: "Japon", value:"jp"}, 
+    				   {name: "Korea", value:"kr"}, {name: "Mexico", value:"mx"}, {name: "Nicaragua", value:"ni"}, {name: "Nueva Zelanda", value:"nz"},
+    				   {name: "Polonia", value:"pl"}, {name: "Portugal", value:"pt"}, {name: "Reino Unido", value:"gb"},{name: "Rusia", value:"ru"}, 
+    				   {name: "Sudafrica", value:"za"}, {name: "Suiza", value:"se"},{name: "Turquia", value:"tr"},{name: "Ucrania", value:"ua"}, 
+    				   {name: "Uruguay", value:"uy"}, {name: "Venezuela", value:"ve"}, {name: "Vietnam", value:"vn"}, {name: "Yemen", value:"ye"},
+    				   {name: "Zimbabwe", value:"zw"}];
+
+    	$scope.genero = [{name: "Todo", value:""},{name: "Artes", value:"1301"}, {name: "Artes", value:"1321"}, {name: "Comedia", value:"1303"}, {name: "Educación", value:"1304"}, {name: "Juegos y Hobbies", value:"1323"},
+    					{name: "Gobierno y Organizaciones", value:"1325"}, {name: "Salud", value:"1307"}, {name: "Niños y Familia", value:"1305"}, {name: "Música", value:"1310"},
+    					{name: "Noticias y Política", value:"1311"}, {name: "Religión", value:"1314"}, {name: "Ciencia y Medicina", value:"1315"}, {name: "Sociedad y cultura", value:"1324"},
+    					{name: "Deportes y Recreación", value:"1316"}, {name: "Tecnología", value:"1318"}, {name: "TV y Películas", value:"1309"}
+    			];
+
+    	$scope.items = [10,25,50,100];
+
+   
 		//Función que obtiene los datos del formulario, recibe como parametro el objeto
 		$scope.obj = new Object();
 		$scope.getData = function (data) {
+			console.log(data, " es la informacion de data")
 			$scope.gather = [];
 			console.log(angular.toJson(data));
 			podcastService.getRss($scope.baseUrl, data)
@@ -43,13 +61,15 @@ angular.module('podcastApp',['ui.router','main.module', 'servicesModule'])
 					$scope.hrefAttr;
 					var a = angular.element(xmlDoc).find('entry').each(function () {
 						$scope.hrefAttr = $(this).find('link').attr('href');
-						$scope.titulo = $(this).find('summary');
+						$scope.titulo = $(this).find('title')[0].innerHTML;
+						$scope.resumen = $(this).find('summary')[0].innerHTML;
 						$scope.artista = $(this).find('im\\:artist, artist').text();
 						$scope.imagen = $(this).find('im\\:image, image')[2].innerHTML;
 						$scope.categoria = $(this).find('category').attr('term');
+						console.log($scope.titulo, " titulo");
 
 						//construye objeto para mostrar en la vista principal de la busqueda.
-						$scope.gather.push({url:$scope.hrefAttr, titulo:$scope.titulo, artista:$scope.artista, imagen:$scope.imagen, categoria:$scope.categoria});
+						$scope.gather.push({url:$scope.hrefAttr, titulo:$scope.titulo, artista:$scope.artista, imagen:$scope.imagen, resumen: $scope.resumen, categoria:$scope.categoria});
 					});
 					console.log(angular.toJson($scope.gather), " si agrego todos");
 				})	
@@ -57,13 +77,15 @@ angular.module('podcastApp',['ui.router','main.module', 'servicesModule'])
 
 		$scope.getPodcast = function (url) {
 			var urlPassed = url;
-			$scope.podcastList = [];
+			$rootScope.podcastList = [];
+			//$scope.prueba2=[1];
 			//url = "https://itunes.apple.com/us/podcast/grammar-girl-quick-dirty-tips/id173429229?mt=2&ign-mpt=uo%3D2";
 			podcastService.getAudio(urlPassed).then(function(promise){
 				//console.log("promesa ", promise.data);
 				var table = promise.data;
 				var t = angular.element(table).find('tr.podcast-episode').each(function() {
-					$scope.descripcion = $(this).children(".release-date").attr('sort-value');
+					$scope.descripcion = $(this).find("td.description").attr('sort-value');
+					$scope.descripcion = $scope.descripcion.replace(/[}|{]/g, "");
 					$scope.album = $(this).attr('album');
 					$scope.tipo = $(this).attr('kind');
 					$scope.audio = $(this).attr('audio-preview-url');
@@ -71,13 +93,26 @@ angular.module('podcastApp',['ui.router','main.module', 'servicesModule'])
 					$scope.audioArtista = $(this).attr('preview-artist');
 					$scope.audioTitulo = $(this).attr('preview-title');
 					$scope.audioDuracion = $(this).attr('preview-title');
-					$scope.podcastList.push({descripcion:$scope.descripcion, album:$scope.audioAlbum, artista:$scope.audioArtista, titulo:$scope.audioTitulo, duracion:$scope.audioDuracion});
-				})
+					$scope.fecha = $(this).find(".release-date").attr('sort-value');
+
+					$rootScope.podcastList.push({fecha:$scope.fecha, descripcion:$scope.descripcion, audio:$scope.audio, album:$scope.audioAlbum, artista:$scope.audioArtista, titulo:$scope.audioTitulo, duracion:$scope.audioDuracion});
+				});
 				console.log(angular.toJson($scope.podcastList), " si agrego todos los audios");
 
 			})
 			
 		}
+
+		//Recurso de audio confiable https
+		$scope.secureAudioUrl = function (url) {
+			return $sce.trustAsResourceUrl(url);
+		};
+
+		// $scope.rscReady = function () {
+		// 	var r = angular.element(document).find('audio').readyState;
+		// 	(r<=2) ? return false : return true
+		// 	//console.log(r, "valor de audio");
+		// };
 	}])
 	.directive('directiveDescargaXml', function () {
 		return {  
--- assets/js/controllers/services/services.js
+++ assets/js/controllers/services/services.js
@@ -2,8 +2,8 @@ angular.module('servicesModule',[])
 	.factory('podcastService',['$http','$q', function ($http, $q) {
 		return {
 			getRss : function (url, data) {
-				return $http.get("https://itunes.apple.com/us/rss/toppodcasts/limit=10/genre=1304/explicit=true/xml"); 
-				//return $http.get("https://itunes.apple.com/"+data.pais+"/rss/toppodcasts/limit="+data.numero+"/genre="+data.genero+"/explicit="+data.contenido+"/xml"); 
+				//return $http.get("https://itunes.apple.com/us/rss/toppodcasts/limit=10/genre=1304/explicit=true/xml"); 
+				return $http.get("https://itunes.apple.com/"+data.pais+"/rss/toppodcasts/limit="+data.numero+"/genre="+data.genero+"/explicit="+data.contenido+"/xml"); 
 			},
 			getAudio : function (url) {
 				var promise =  $http.get(url)
